f9db00bb99108db4fb3c6a09a9fb73aef772237d
Emerson Rafael <emersona7x@hotmail.com>
2021-08-15 17:56:28 -0300

    [ADD] INCLUSÃO DOS UTILS CONTENDO AS FUNÇÕES PARA LEITURA DA IMAGEM

---
+++ utils/image_read.py
@@ -0,0 +1,39 @@
+"""
+
+    FUNÇÕES PARA LEITURA DA IMAGEM.
+
+    # Arguments
+        object                  - Required : Imagem para leitura/visualização (String | Object)
+    # Returns
+
+"""
+
+
+from inspect import stack
+
+from iglovikov_helper_functions.utils.image_utils import load_rgb
+
+
+def read_image_rgb(caminho_imagem):
+
+    """
+
+        FUNÇÃO PARA LEITURA DE UMA IMAGEM.
+
+        # Arguments
+            caminho_imagem       - Required : Caminho da imagem a ser lida (String)
+        # Returns
+            img                  - Required : Imagem após leitura (Array)
+
+    """
+
+    # INICIANDO O OBJETO DA IMAGEM
+    img = None
+
+    try:
+        # A LEITURA É FEITA EM FORMATO RGB
+        img = load_rgb(caminho_imagem)
+    except Exception as ex:
+        print(ex)
+
+    return img
