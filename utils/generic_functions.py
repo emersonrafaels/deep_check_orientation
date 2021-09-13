1aa12ca00aac5a816484b239314c989cc7ccd539
Emerson Rafael <emersona7x@hotmail.com>
2021-09-13 15:31:03 -0300

    [ADD] INCLUSÃO DA FUNÇÃO DE INTERSSECÃO ENTRE DUAS LISTAS GENÉRICAS

--- utils/generic_functions.py
+++ utils/generic_functions.py
@@ -19,6 +19,19 @@ class generic_functions():
     @staticmethod
     def converte_int(valor_para_converter):
 
+        """
+
+            FUNÇÃO GENÉRICA PARA CONVERTER UM VALOR PARA FORMATO INTEIRO.
+
+
+            # Arguments
+                valor_para_converter              - Required : Valor para converter (Object)
+
+            # Returns
+                valor_para_converter              - Required : Valor após conversão (Integer)
+
+        """
+
         try:
             if isinstance(valor_para_converter, int):
                 return valor_para_converter
@@ -27,3 +40,30 @@ class generic_functions():
         except Exception as ex:
             print(ex)
             return None
+
+
+    @staticmethod
+    def get_list_intersecction(list1, list2):
+
+        """
+
+            OBTÉM A INTERCÇÃO ENTRE DUAS LISTAS
+
+            # Arguments
+                list1                       - Required : Lista de dados 1 (List)
+                list2                       - Required : Lista de dados 2 (List)
+
+            # Returns
+                result_intersecction        - Required : Lista com os dados interseccionados (List)
+
+        """
+
+        # INICIANDO A LISTA DE RESULTADO DA INTERSECÇÃO
+        result_intersecction = []
+
+        try:
+            result_intersecction = list(set(list1).intersection(set(list2)))
+        except Exception as ex:
+            print(ex)
+
+        return result_intersecction
