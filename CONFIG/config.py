6c98b29f88b29fe2e707b700cbd6a08e474f9933
Emerson Rafael <emersona7x@hotmail.com>
2021-12-07 20:13:58 -0300

    [CHG] REFATORAÇÃO DO CONFIG

--- CONFIG/config.py
+++ CONFIG/config.py
@@ -1,14 +1,6 @@
 import os
 
-from dynaconf import Dynaconf, settings
-
-settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=['settings.yaml', '.secrets.yaml'],
-)
-
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load this files in the order.
+from dynaconf import settings
 
 # OBTENDO O DIRETÓRIO CONTENDO O BANCO DE DADOS
 DIR_MODEL_WORDS = os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')), settings.DIR_MODEL_WORDS)
