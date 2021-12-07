b2ca7bc17845ad4e9e4653a625b115011ab47802
Emerson Rafael <emersona7x@hotmail.com>
2021-12-07 20:00:50 -0300

    [CHG] UNIÃO DOS ARQUIVOS DE CONFIGURAÇÃO (HAVIAM DOIS CONFIG.PY)

--- CONFIG/config.py
+++ CONFIG/config.py
@@ -1,5 +1,14 @@
-from dynaconf import settings
 import os
+
+from dynaconf import Dynaconf, settings
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=['settings.yaml', '.secrets.yaml'],
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load this files in the order.
 
 # OBTENDO O DIRETÓRIO CONTENDO O BANCO DE DADOS
 DIR_MODEL_WORDS = os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')), settings.DIR_MODEL_WORDS)
